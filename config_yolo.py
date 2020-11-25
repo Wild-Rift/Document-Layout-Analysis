9625864aca8905cab1011f6c8521d5787929e204
Phạm Văn Tiến (VNCDLL-CTPTNLVKHDL) <tienpv12@vingroup.net>
2020-11-25 16:39:31 +0700

    add bootstrap file from enormous dataset

---
+++ config_yolo.py
@@ -0,0 +1,17 @@
+import numpy as np
+import tensorflow as tf
+import os
+from shutil import copyfile
+
+def bootstrap(image_dir, label_dir, dst_dir, probability=0.2):
+    for image_file_name in os.listdir(image_dir):
+        if np.random.random() < probability:
+            image_file_name_without_extension = image_file_name.split('\.')[0]
+            image_file = image_file_name_without_extension + '.jpg'
+            label_file = image_file_name_without_extension + '.txt'
+            image_path = os.join(image_dir, image_file)
+            label_path = os.join(label_dir, label_file)
+            dst_image_path = os.join(dst_dir, image_file)
+            dst_label_path = os.join(dst_dir, label_file)
+            copyfile(image_path, dst_image_path)
+            copyfile(label_path, dst_label_path)
